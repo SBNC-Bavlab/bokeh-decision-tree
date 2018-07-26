2e8ef91ca02619d6115822f8e2922acdc6cc25a9
evrimozel <evrozel@gmail.com>
2018-07-26 10:22:15 +0300

    bugs

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from Decision_Tree.ID3_Decision_Tree.generate_bokeh_data import get_bokeh_data
 from math import atan, pi
 from Decision_Tree.Plot.get_data import set_new_dataset, get_all_colors
 from Decision_Tree.Plot.instance import Instance
-from bokeh.io import show
 from bokeh.io import curdoc
 
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -158,22 +157,6 @@ def modify_test_percentage(_attr, _old, new):
 
 
 dataset_slider.on_change('value', modify_test_percentage)
-
-
-def toggle_mode_set(new):
-    """
-    toggles settings
-    """
-    p.select(name="circles").visible = not new
-    p.select(name="detailed_text").visible = new
-
-    best_root_plot.select(name="circles").visible = not new
-    best_root_plot.select(name="detailed_text").visible = new
-
-    if decision_button.label == "Show Labels":
-        p.select(name="decision_text").visible = not new
-        best_root_plot.select(name="decision_text").visible = not new
-    decision_button.disabled = new
 
 
 def turn_decision_off(new):
