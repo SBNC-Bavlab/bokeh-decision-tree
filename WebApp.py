5ed051cfdbec4cf7bd7b1fec536aa63982673df2
liber <e2035632@ceng.metu.edu.tr>
2018-07-26 10:07:23 +0300

    Merge branch 'master' of https://github.com/OnurYigitArpali/bokeh-decision-tree
    # Conflicts:
    #	templates/index.html

--- WebApp.py
+++ WebApp.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 from bokeh.embed import server_document
 from bokeh.server.server import Server
 from tornado.ioloop import IOLoop
+from threading import Thread
 
 from Decision_Tree.Plot.decision_tree import create_figure
 app = Flask(__name__)
@@ -11,7 +12,6 @@ app = Flask(__name__)
 def modify_doc(doc):
     # Create the plot
     plot = create_figure()
-    print("here234")
     # Embed plot into HTML via Flask Render
     doc.add_root(plot)
 
@@ -29,17 +29,12 @@ def bkapp_page():
 
 
 def bk_worker():
-    server = Server({'/bkapp': modify_doc}, io_loop=IOLoop(), allow_websocket_origin=["localhost:8000"])
+    server = Server({'/bkapp': modify_doc}, io_loop=IOLoop(), allow_websocket_origin=["localhost:5000"])
     server.start()
     server.io_loop.start()
 
 
-from threading import Thread
 Thread(target=bk_worker).start()
 
 if __name__ == '__main__':
-    print('Opening single process Flask app with embedded Bokeh application on http://localhost:8000/')
-    print()
-    print('Multiple connections may block the Bokeh app in this configuration!')
-    print('See "flask_gunicorn_embed.py" for one way to run multi-process')
-    app.run(port=8000)
+    app.run()
